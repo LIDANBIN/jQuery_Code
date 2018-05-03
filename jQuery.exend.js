049b6fbf54a74e1dd58c99a2b692a12c659432ca
LIDANBIN <straberry2@outlook.com>
2018-05-03 18:04:46 +0800

    'extend'

--- jQuery.exend.js
+++ jQuery.exend.js
@@ -1,11 +1,28 @@
-jQuery.extend = jQuery.fn.extend = function() { // 用于合并两个或多个对象的属性到第一个对象
+// 用于合并两个或多个对象的属性到第一个对象
+// jQuery.extend[deep,] target, object1[, objectN] = jQuery.fn.extend([deep,] target, object1[, ...objectN])
+// deep是可选的布尔值，表示是否深度合并（即递归合并），默认不递归。
+// 如果第一个参数是一个数组或对象，它会被第二个或者后面参数的同名属性完全覆盖。
+// target是目标对象，object1和objectN是源对象。如果仅提供一个对象，那么target将被忽略。
+/**
+ * 执行步骤如下：
+ * 1、修正参数deep、target、源对象的起始下标。
+ * 2、逐个遍历源对象。
+ *  ⅰ、遍历源对象的属性。
+ *  ⅱ、覆盖目标对象的同名属性；如果是深度合并，则先递归调用jQuery.extend。
+ */
+// 因为参数是不确定的，可以有任意多个，所以没有列出可接受的参数。
+jQuery.extend = jQuery.fn.extend = function() {
+    // 定义局部变量：options：指向某个源对象；name：表示某个源对象的某个属性名；src：表示目标对象的某个属性的原始值；
+    // copy：表示某个源对象的某个属性的值；copyIsArray：指示变量copy是否是数组；clone：表示深度复制时原始值的修正值；
+    // target：指向目标对象；i：表示源对象的起始下标；length：表示参数的个数，用于修正变量target；deep：指示是否执行深度复制。
 	var options, name, src, copy, copyIsArray, clone,
 		target = arguments[0] || {},
 		i = 1,
 		length = arguments.length,
 		deep = false;
 
-	// Handle a deep copy situation
+    // Handle a deep copy situation
+    // 修正目标对象target、源对象起始下标i
 	if ( typeof target === "boolean" ) {
 		deep = target;
 		target = arguments[1] || {};
